dd488c8f5c55c205adaab2574fce19dc56ea2b67
carlos <padron.ca@gmail.com>
2025-01-24 21:47:50 +0000

    adding indexes

--- upload_data_local.sh
+++ upload_data_local.sh
@@ -1,3 +1,5 @@
 psql -d gis -f table_definitions.sql;
 psql -d gis -c "\COPY os.open_uprn_white_horse FROM 'data/open_uprn_white_horse.csv' DELIMITER '|' CSV HEADER";  
 psql -d gis -c "\COPY os.code_point_open_white_horse FROM 'data/code_point_open_white_horse.csv' DELIMITER '|' CSV HEADER";  
+psql -d gis -c "CREATE INDEX uprn_wh_gis ON os.open_uprn_white_horse USING gist(geom);"
+psql -d gis -c "CREATE INDEX cp_wh_gis ON os.code_point_open_white_horse USING gist(geom);"
