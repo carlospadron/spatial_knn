feb93daa9478c2e75797b0a40e566a40c14a43b2
carlos <padron.ca@gmail.com>
2025-06-21 17:08:53 +0100

    fix: update file paths for data sources and add WKB geometry columns in spatial data preparation script

--- prepare_data_spark.py
+++ prepare_data_spark.py
@@ -22,14 +22,14 @@ spark = SedonaContext.create(config)
 
 # %%
 # Read OS UPRN CSV
-uprn = spark.read.option("header", True).csv("data/osopenuprn_202506.csv")
+uprn = spark.read.option("header", True).csv("data/raw/osopenuprn_202506.csv")
 uprn = uprn.withColumn("X_COORDINATE", col("X_COORDINATE").cast("double")) \
            .withColumn("Y_COORDINATE", col("Y_COORDINATE").cast("double"))
 
 # %%
 # Read local authorities and codepoint from GeoPackage
-local_auth = spark.read.format("geopackage").option("tableName", "district_borough_unitary").load("data/bdline_gb.gpkg")
-codepoint = spark.read.format("geopackage").option("tableName", "codepoint").load("data/codepo_gb.gpkg")
+local_auth = spark.read.format("geopackage").option("tableName", "district_borough_unitary").load("data/raw/bdline_gb.gpkg")
+codepoint = spark.read.format("geopackage").option("tableName", "codepoint").load("data/raw/codepo_gb.gpkg")
 
 # %%
 # Create geometry for UPRN points
@@ -70,7 +70,14 @@ cp_wh = cp_wh.groupBy("geometry").agg(
 cp_wh = cp_wh.withColumnRenamed("geometry", "geom")
 
 # %%
-# Write output as ORC
+# Add WKB geometry column for UPRN
+uprn_wh = uprn_wh.withColumn("geom_wkb", expr("ST_AsBinary(geom)"))
+
+# Add WKB geometry column for CodePoint
+cp_wh = cp_wh.withColumn("geom_wkb", expr("ST_AsBinary(geom)"))
+
+# %%
+# Write output as ORC (with WKB geometry)
 uprn_wh.write.mode("overwrite").orc("data/open_uprn_white_horse.orc")
 cp_wh.write.mode("overwrite").orc("data/code_point_open_white_horse.orc")
 
